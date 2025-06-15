b56cf3a99cc5c19ffdeb322eb2dd083baac6f332
haruna <harunahafiz1980@gmail.com>
2025-06-15 07:01:07 +0100

    first_answer

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def deposit(self, amount):
+        self.amount += amount
+
+    def withdraw(self, amount):
+        if amount > self.amount:
+            return False
+        else:
+            self.amount -= amount
+            return True
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.amount}")
+
